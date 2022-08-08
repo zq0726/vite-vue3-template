0b9cc240d24fc66a3ff259c01f83b6b319b2ed13
赵庆 <805322803@qq.com>
2022-08-08 16:04:31 +0800

    perf(deps): git  提交规范添加
    about 功能提交

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="container">home</div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+console.log('太难了，一点也不简单')
+</script>
 <style scoped></style>
